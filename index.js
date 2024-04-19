7278279b07796b5a547a074ea3f75f71a9796d9e
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-19 21:37:40 +0600

    server user data show

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
 
+const users = [
+    {id: 1,name: "zahid", email: "zahid@gmail.com"},
+    {id: 2,name: "said", email: "said@gmail.com"},
+    {id: 3,name: "tohid", email: "tohid@gmail.com"}
+]
 
 app.get('/', (req, res) => {
     res.send('hay i am default data')
 })
+
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+
 
 
 app.listen(port, () => {
